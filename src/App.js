18dbe0cd1dd0da54f259986da0f7540a7cc660d8
Oliver Joisten <o.joisten@live.se>
2023-06-13 21:18:06 +0200

    🔒 chore(server.js): refactor API_KEY to a constant 🔧 fix(server.js): change endpoint from /completion to /completions and use request body for messages Refactored the API_KEY to a constant for better readability and maintainability. Changed the endpoint from /completion to /completions to improve consistency with the naming conventions. The request body is now used for messages instead of a hardcoded value.
    🎨 style(App.js): reformat code and add whitespace for readability
    ✨ feat(App.js): add functionality to create new chat, handle click on chat history, and display chat messages
    The code has been reformatted and whitespace has been added to improve readability. The functionality to create a new chat, handle clicks on chat history, and display chat messages has been added. The user can now create a new chat, click on previous chats to view them, and send messages to ChatGPT to receive responses. The chat messages are displayed in the feed section of the UI.
    
    🎨 style(index.css): add styling to the feed section
    This commit adds styling to the feed section of the application. The feed section now has a scrollbar with a custom style for Webkit, Firefox, Edge, and IE browsers. The feed list items now have a background color of #444654, and the text color is now rgba(255, 255, 255, 0.8). The role paragraph now has a minimum width of 100px.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,122 @@
-const  App = () => {
+import { useState, useEffect, useRef } from "react";
+
+const App = () => {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+  const inputRef = useRef(null);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  };
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue("");
+  };
 
   const getMessages = async () => {
     const options = {
-      method: 'POST',
-      body :JSON.stringify({
-        message: "Hello"
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    }
+        "Content-Type": "application/json",
+      },
+    };
     try {
-      const response = await fetch('http://localhost:8000/completion', options) 
-      const data = response.json()
-      console.log(data)
+      const response = await fetch("http://localhost:8000/completions", options);
+      const data = await response.json();
+      setMessage(data.choices[0].message);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
+
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content: value,
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
+    }
+  }, [message, currentTitle]);
+
+  console.log(previousChats);
+
+  const currentChat = previousChats.filter((previousChats) => previousChats.title === currentTitle);
+  const uniqueTitles = Array.from(new Set(previousChats.map((previousChat) => previousChat.title)));
+  console.log(uniqueTitles);
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      getMessages();
+    }
+  };
 
   return (
-    <div className="app">   
+    <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
+        <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
-          <li>BLUGH</li>
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
+            </li>
+          ))}
         </ul>
         <nav>
           <p>Made by The-R4V3N</p>
-          </nav>
-        </section> 
+        </nav>
+      </section>
       <section className="main">
-      <h1>Your AI assistant</h1>
-      <ul className="feed">
-
-      </ul>
-      <div className="bottom-section">
-        <div className="input-container">
-          <input/>
-          <div id="submit" onClick={getMessages}>➢</div>
+        {!currentTitle && <h1>Your AI assistant</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input
+              ref={inputRef}
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              onKeyPress={handleKeyPress}
+            />
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
+          </div>
+          <p className="info">
+            ChatGPT may produce inaccurate information about people, places, or facts.{" "}
+            <a href="https://help.openai.com/en/articles/6825453-chatgpt-release-notes">ChatGPT May 24 Version</a>
+          </p>
         </div>
-        <p className="info">ChatGPT may produce inaccurate information about people, places, or facts. <a href="https://help.openai.com/en/articles/6825453-chatgpt-release-notes">ChatGPT May 24 Version</a></p>
-      </div>
-      </section> 
+      </section>
     </div>
   );
-}
+};
 
 export default App;
